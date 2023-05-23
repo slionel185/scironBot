9f18b4debf7a92b2aac8c6f232164f603000f332
Seth <slionel1850@gmail.com>
2023-05-23 02:18:11 +0000

    Handle Mode Change

--- src/controllers/actions/chat.ts
+++ src/controllers/actions/chat.ts
@@ -3,9 +3,25 @@ import type { User, Command } from '@prisma/client'
 import { bot } from '@/server'
 
 const chat = async (channel: string, command: Command) => {
-    const chatArgs = command
+    const args = command.args.split(' ')
 
-    console.log(chatArgs)
+    try {
+        await bot.slowoff(channel)
+        await bot.emoteonlyoff(channel)
+        await bot.subscribersoff(channel)
+        await bot.followersonlyoff(channel)
+
+        switch (args[0]) {
+            case 'SLOW': return bot.slow(channel)
+            case 'EMOTE': return bot.emoteonly(channel)
+            case 'FOLLOWERS': return bot.followersonly(channel)
+            case 'SUBSCRIBERS': return bot.subscribers(channel)
+            case 'NORMAL': return
+        }
+
+    } catch(err) {
+        await bot.say(channel, 'There was an error -_- I might fix it later.')
+    }
 }
 
 export default chat
