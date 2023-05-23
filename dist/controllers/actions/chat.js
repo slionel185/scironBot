c9a89e4730b71948558adb1e1013adb53bd10cd0
Seth <slionel1850@gmail.com>
2023-05-23 02:18:38 +0000

    HandleModeChangeBuild

--- dist/controllers/actions/chat.js
+++ dist/controllers/actions/chat.js
@@ -9,8 +9,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = require("../../server");
 const chat = (channel, command) => __awaiter(void 0, void 0, void 0, function* () {
-    const chatArgs = command;
-    console.log(chatArgs);
+    const args = command.args.split(' ');
+    try {
+        yield server_1.bot.slowoff(channel);
+        yield server_1.bot.emoteonlyoff(channel);
+        yield server_1.bot.subscribersoff(channel);
+        yield server_1.bot.followersonlyoff(channel);
+        switch (args[0]) {
+            case 'SLOW': return server_1.bot.slow(channel);
+            case 'EMOTE': return server_1.bot.emoteonly(channel);
+            case 'FOLLOWERS': return server_1.bot.followersonly(channel);
+            case 'SUBSCRIBERS': return server_1.bot.subscribers(channel);
+            case 'NORMAL': return;
+        }
+    }
+    catch (err) {
+        yield server_1.bot.say(channel, 'There was an error -_- I might fix it later.');
+    }
 });
 exports.default = chat;
